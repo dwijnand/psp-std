c4de6c6f5e854cdbbfb4befc63b2888c26f458b7
Paul Phillips <paulp@improving.org>
2016-04-29 10:54:48 -0700

    Same.

--- std/src/main/scala/ops/TypeClass.scala
+++ std/src/main/scala/ops/TypeClass.scala
@@ -26,10 +26,10 @@ final class OrderOps[A](val lhs: A) extends AnyVal {
   def max(rhs: A)(implicit ord: Order[A]): A = if (this > rhs) lhs else rhs
 }
 final class AlgebraOps[A](val lhs: A) extends AnyVal {
-  def ^(rhs: A)(implicit z: BooleanAlgebra[A]): A       = (lhs || rhs) && !(lhs && rhs) // xor
-  def implies(rhs: A)(implicit z: BooleanAlgebra[A]): A = !lhs || rhs
-  def && (rhs: A)(implicit z: BooleanAlgebra[A]): A     = if (isOne) rhs else if (rhs.isOne) lhs else if (lhs.isZero || rhs.isZero) z.zero else z.and(lhs, rhs)
-  def || (rhs: A)(implicit z: BooleanAlgebra[A]): A     = if (isZero) rhs else if (rhs.isZero) lhs else if (lhs.isOne || rhs.isOne) z.one else z.or(lhs, rhs)
+  // def ^(rhs: A)(implicit z: BooleanAlgebra[A]): A       = (lhs || rhs) && !(lhs && rhs) // xor
+  // def implies(rhs: A)(implicit z: BooleanAlgebra[A]): A = !lhs || rhs
+  // def && (rhs: A)(implicit z: BooleanAlgebra[A]): A     = if (isOne) rhs else if (rhs.isOne) lhs else if (lhs.isZero || rhs.isZero) z.zero else z.and(lhs, rhs)
+  // def || (rhs: A)(implicit z: BooleanAlgebra[A]): A     = if (isZero) rhs else if (rhs.isZero) lhs else if (lhs.isOne || rhs.isOne) z.one else z.or(lhs, rhs)
   def unary_!(implicit z: BooleanAlgebra[A]): A         = if (isZero) z.one else if (isOne) z.zero else z.complement(lhs)
   def isZero(implicit z: BooleanAlgebra[A]): Boolean    = z.zero id_== lhs
   def isOne(implicit z: BooleanAlgebra[A]): Boolean     = z.one id_== lhs
