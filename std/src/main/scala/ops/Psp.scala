980926ace1f52bbef02482f0fa34f7d4b17449d7
Paul Phillips <paulp@improving.org>
2016-04-29 10:47:56 -0700

    Same.

--- std/src/main/scala/ops/Psp.scala
+++ std/src/main/scala/ops/Psp.scala
@@ -10,7 +10,7 @@ final class DirectOps[A](val xs: Direct[A]) extends AnyVal {
   def head: A = apply(0)
   def last: A = apply(lastIndex)
   def tail    = xs.drop(1)
-  def init    = xs.dropRight(1)
+  // def init    = xs.dropRight(1)
 
   def reverse: Direct[A]  = Direct reversed xs
   def apply(i: Index): A  = xs elemAt i
@@ -20,7 +20,7 @@ final class DirectOps[A](val xs: Direct[A]) extends AnyVal {
   def containsIndex(index: Index): Boolean = indices containsInt index.getInt
 
   @inline def foreachIndex(f: Index => Unit): Unit  = if (xs.size.get > 0L) lowlevel.ll.foreachConsecutive(0, lastIndex.getInt, i => f(Index(i)))
-  @inline def foreachIntIndex(f: Int => Unit): Unit = if (xs.size.get > 0L) lowlevel.ll.foreachConsecutive(0, lastIndex.getInt, f)
+  // @inline def foreachIntIndex(f: Int => Unit): Unit = if (xs.size.get > 0L) lowlevel.ll.foreachConsecutive(0, lastIndex.getInt, f)
 }
 
 final class ForeachOps[A](val xs: Foreach[A]) {
@@ -31,33 +31,33 @@ final class ForeachOps[A](val xs: Foreach[A]) {
 final class DocOps(val lhs: Doc) extends AnyVal {
   def doc: Doc                             = lhs
   def render(implicit z: Renderer): String = z show lhs
-  def isEmpty: Boolean                     = lhs eq emptyValue[Doc]
+  // def isEmpty: Boolean                     = lhs eq emptyValue[Doc]
 
   def ~(rhs: Doc): Doc   = Doc.Cat(lhs, rhs)
-  def <>(rhs: Doc): Doc  = if (lhs.isEmpty) rhs else if (rhs.isEmpty) lhs else lhs ~ rhs
-  def <+>(rhs: Doc): Doc = if (lhs.isEmpty) rhs else if (rhs.isEmpty) lhs else lhs ~ " " ~ rhs
+  // def <>(rhs: Doc): Doc  = if (lhs.isEmpty) rhs else if (rhs.isEmpty) lhs else lhs ~ rhs
+  // def <+>(rhs: Doc): Doc = if (lhs.isEmpty) rhs else if (rhs.isEmpty) lhs else lhs ~ " " ~ rhs
 }
 final class ExMapOps[K, V](xs: ExMap[K, V]) {
   type Entry = K -> V
 
   def keys: View[K]          = keySet.m
-  def values: View[V]        = keyVector map xs.lookup
+  // def values: View[V]        = keyVector map xs.lookup
   def keySet: ExSet[K]       = xs.lookup.keys
   def keyVector: Vec[K]      = keys.toVec
   def entries: ZipView[K, V] = keyVector mapZip xs.lookup
 
-  def filterValues(p: ToBool[V]): ExMap[K, V] = xs filterKeys (k => p(xs(k)))
+  // def filterValues(p: ToBool[V]): ExMap[K, V] = xs filterKeys (k => p(xs(k)))
 }
 final class ExSetOps[A](xs: ExSet[A]) {
-  def add(x: A): ExSet[A]                = xs + x
+  // def add(x: A): ExSet[A]                = xs + x
   def filter(p: ToBool[A]): ExSet[A]     = ExSet.Filtered(xs, p)
   def union(that: ExSet[A]): ExSet[A]    = ExSet.Union(xs, that)
-  def mapOnto[B](f: A => B): ExMap[A, B] = ExMap(xs, Fun(f))
+  // def mapOnto[B](f: A => B): ExMap[A, B] = ExMap(xs, Fun(f))
 }
 
 final class TimesBuilder(val times: Precise) {
   def const[A](elem: A): Each[A]   = Each const elem take times
-  def eval[A](body: => A): Each[A] = Each continually body take times
+  // def eval[A](body: => A): Each[A] = Each continually body take times
 }
 
 final class PreciseOps(val size: Precise) {
@@ -66,24 +66,24 @@ final class PreciseOps(val size: Precise) {
   def indices: IndexRange = indexRange(0, size.getInt)
   def lastIndex: Index    = Index(size.getLong - 1)  // effectively maps both undefined and zero to no index.
 
-  def + (n: Precise): Precise              = size + n.get
+  // def + (n: Precise): Precise              = size + n.get
   def - (n: Precise): Precise              = size - n.get
   def containsIndex(index: Index): Boolean = indices containsInt index.getInt
 
   def min(rhs: Precise): Precise = if (size <= rhs) size else rhs
-  def max(rhs: Precise): Precise = if (size >= rhs) size else rhs
+  // def max(rhs: Precise): Precise = if (size >= rhs) size else rhs
 
-  @inline def foreachIndex(f: Index => Unit): Unit  = if (size.get > 0L) lowlevel.ll.foreachConsecutive(0, lastIndex.getInt, i => f(Index(i)))
-  @inline def foreachIntIndex(f: Int => Unit): Unit = if (size.get > 0L) lowlevel.ll.foreachConsecutive(0, lastIndex.getInt, f)
+  // @inline def foreachIndex(f: Index => Unit): Unit  = if (size.get > 0L) lowlevel.ll.foreachConsecutive(0, lastIndex.getInt, i => f(Index(i)))
+  // @inline def foreachIntIndex(f: Int => Unit): Unit = if (size.get > 0L) lowlevel.ll.foreachConsecutive(0, lastIndex.getInt, f)
 }
 
 final class InputStreamOps(val in: InputStream) extends AnyVal {
-  def buffered: BufferedInputStream = in match {
-    case in: BufferedInputStream => in
-    case _                       => new BufferedInputStream(in)
-  }
-  def slurp(): Array[Byte]             = lowlevel.Streams slurp buffered
-  def slurp(len: Precise): Array[Byte] = lowlevel.Streams.slurp(buffered, len)
+  // def buffered: BufferedInputStream = in match {
+  //   case in: BufferedInputStream => in
+  //   case _                       => new BufferedInputStream(in)
+  // }
+  // def slurp(): Array[Byte]             = lowlevel.Streams slurp buffered
+  // def slurp(len: Precise): Array[Byte] = lowlevel.Streams.slurp(buffered, len)
 }
 
 final class SizeOps(val lhs: Size) extends AnyVal {
@@ -103,7 +103,7 @@ final class SizeOps(val lhs: Size) extends AnyVal {
     case x: Atomic      => x
   }
 
-  def slice(range: VindexRange): Size = (this - range.startInt) min range.size
+  // def slice(range: VindexRange): Size = (this - range.startInt) min range.size
 
   /** For instance taking the union of two sets. The new size is
    *  at least the size of the larger operand, but at most the sum
@@ -135,8 +135,8 @@ final class FunOps[A, B](val f: Fun[A, B]) extends AnyVal {
   outer =>
 
   def get(x: A): Option[B]            = if (f isDefinedAt x) Some(f(x)) else None
-  def getOr(key: A, alt: => B): B     = get(key) getOrElse alt
-  def orElse(g: Fun[A, B]): Fun[A, B] = OrElse(f, g)
+  // def getOr(key: A, alt: => B): B     = get(key) getOrElse alt
+  // def orElse(g: Fun[A, B]): Fun[A, B] = OrElse(f, g)
   def mapIn[C](g: C => A): Fun[C, B]  = AndThen(Fun(g), f)
   def mapOut[C](g: B => C): Fun[A, C] = AndThen(f, Fun(g))
 
