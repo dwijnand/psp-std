1c6ddecd27c6ebfb0e4bfa5752b581fd3c10531f
Paul Phillips <paulp@improving.org>
2016-05-05 17:08:12 -0700

    Empty.

--- std/src/main/scala/Empty.scala
+++ std/src/main/scala/Empty.scala
@@ -16,25 +16,22 @@ object Empty {
   def const[A](empty: A): Const[A]   = new Const[A](empty)
 
   final class Throws[A](msg: String) extends Empty[A] { def empty: A = abort(msg) }
-  final class Impl[A](expr: => A) extends Empty[A] { def empty: A = expr }
-  final class Const[A](val empty: A) extends AnyVal with Empty[A]
+  final class Impl[A](expr: => A) extends Empty[A]    { def empty: A = expr       }
+  final class Const[A](val empty: A) extends Empty[A] {                           }
 }
 
 trait StdEmpty {
   implicit def emptyCanBuild[R](implicit z: CanBuild[_, R]): Empty[R] = Empty(z().result)
   implicit def emptyEach[R](implicit z: Builds[_, R]): Empty[R]       = Empty(z build vec())
-  // implicit def emptyJavaList[A] : Empty[jList[A]]                     = Empty(new jArrayList[A])
-  // implicit def emptyJavaMap[K, V] : Empty[jMap[K, V]]                 = Empty(new jHashMap[K, V])
-  // implicit def emptyJavaSet[A] : Empty[jSet[A]]                       = Empty(new jHashSet[A])
   implicit def emptyOption[A] : Empty.Const[Option[A]]                = Empty const None
   implicit def emptyOptional[A] : Empty[jOptional[A]]                 = Empty(java.util.Optional.empty[A]())
   implicit def emptyTuple[A: Empty, B: Empty]: Empty[(A, B)]          = Empty(pair(emptyValue[A], emptyValue[B]))
   implicit def emptyView[A, R] : Empty[AtomicView[A, R]]              = Empty(new LinearView(Pnil))
 
-  implicit lazy val emptyFile: Empty.Const[jFile]           = Empty const NoFile
-  implicit lazy val emptyIndex: Empty.Const[Index]          = Empty const Index.invalid
-  implicit lazy val emptyIndexRange: Empty.Const[VdexRange] = Empty const indexRange(0, 0)
-  implicit lazy val emptyNth: Empty.Const[Nth]              = Empty const Nth.invalid
-  implicit lazy val emptyPath: Empty.Const[jPath]           = Empty const NoPath
-  implicit lazy val emptyString: Empty.Const[String]        = Empty const ""
+  implicit lazy val emptyFile: Empty.Const[jFile]          = Empty const NoFile
+  implicit lazy val emptyIndex: Empty.Const[Index]         = Empty const Index.invalid
+  implicit lazy val emptyNth: Empty.Const[Nth]             = Empty const Nth.invalid
+  implicit lazy val emptyPath: Empty.Const[jPath]          = Empty const NoPath
+  implicit lazy val emptyString: Empty.Const[String]       = Empty const ""
+  implicit lazy val emptyVdexRange: Empty.Const[VdexRange] = Empty const indexRange(0, 0)
 }
