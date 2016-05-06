46e2fdf9d0c109a18aa2ab808a31f8e3d11eeceb
Paul Phillips <paulp@improving.org>
2016-05-06 09:47:35 -0700

    Travis cross build.

--- .ci/run.sh
+++ .ci/run.sh
@@ -5,17 +5,33 @@ set -o pipefail
 set -e
 GREP="egrep --line-buffered"
 
-# Look ma I'm testing pipefail.
-if /usr/bin/false | cat; then
-  echo "Failing pipe didn't fail!" && exit 1
-fi
+run211 () {
+  # Look ma I'm testing pipefail.
+  if false | cat; then
+    echo "Failing pipe didn't fail!" && exit 1
+  fi
 
-runTests () {
   if sbt console <.ci/repl.txt |& grep -q 'Compilation Failed'; then
     echo "console smoke test encountered error" && exit 1
   fi
 
-  sbt -batch -no-colors cover
+  sbt ++$TRAVIS_SCALA_VERSION -batch -no-colors cover
+}
+run212 () {
+  # High fucking tech right here.
+  # https://github.com/scoverage/sbt-scoverage/issues/96
+  SED="sed"
+  which gsed >/dev/null && SED="gsed"
+  $SED --in-place 's/.*coverage.*//' ./project/plugins.sbt
+
+  sbt ++$TRAVIS_SCALA_VERSION -batch -no-colors test
+  git checkout HEAD -- ./project/plugins.sbt
+}
+runTests () {
+  case $TRAVIS_SCALA_VERSION in
+    2.12*) run212 ;;
+        *) run211 ;;
+  esac
 }
 
 runTests \
