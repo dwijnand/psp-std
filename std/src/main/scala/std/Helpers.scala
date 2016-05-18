151fd698517e674371c892e0a0f0e9a4f643cacc
Paul Phillips <paulp@improving.org>
2016-05-18 08:35:38 -0700

    Hash Eq Ord reconciliation.

--- std/src/main/scala/std/Helpers.scala
+++ std/src/main/scala/std/Helpers.scala
@@ -7,15 +7,10 @@ import api._, all._
   *  where it can either be inferred from an existing value or
   *  supplied directly.
   */
-final class OrderBy[A] { def apply[B](f: A => B)(implicit z: Order[B]): Order[A] = Order[A]((x, y) => z.cmp(f(x), f(y))) }
-final class ShowBy[A] { def apply[B](f: A => B)(implicit z: Show[B]): Show[A]    = Show[A](f andThen z.show) }
-final class HashBy[A] { def apply[B](f: A => B)(implicit z: Hash[B]): Hash[A]    = Eq.hash[A]((x, y) => f(x) === f(y))(f andThen z.hash) }
-final class EqBy[A] { def apply[B](f: A => B)(implicit z: Eq[B]): Eq[A]          = Eq[A]((x, y) => f(x) === f(y)) }
-
-final class HashEqOrdBy[A] {
-  def apply[B](f: A => B)(implicit heq: Hash[B], ord: Order[B]): HashEqOrd[A] =
-    HashEqOrd(ord.eqv _ on f, ord.cmp _ on f, f andThen heq.hash)
-}
+final class OrderBy[A] { def apply[B](f: A => B)(implicit z: Order[B]): Order[A]         = z on f }
+final class ShowBy[A]  { def apply[B](f: A => B)(implicit z: Show[B]): Show[A]           = z on f }
+final class HashBy[A]  { def apply[B](f: A => B)(implicit z: Hash[B]): Hash[A]           = z on f }
+final class AllBy[A]   { def apply[B](f: A => B)(implicit z: HashEqOrd[B]): HashEqOrd[A] = z on f }
 
 class RemakeHelper[R](xs: R) {
   def apply[A](f: R => View[A])(implicit z: Builds[A, R]): R = z build f(xs)
